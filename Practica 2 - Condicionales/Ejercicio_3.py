ed303fa917a8fbbaa2d3c4527dc390d05d324256
Thiago Vildosa <tvildosa@gmail.com>
2023-09-03 21:51:47 -0300

    add ejercicio_3

---
+++ Practica 2 - Condicionales/Ejercicio_3.py
@@ -0,0 +1,49 @@
+# Para cada uno de los siguientes programas indicar qué se imprime cuando se ejecuta
+# a)
+a = 10
+if a != 0:
+    print("perro")
+# Salida: perro
+
+# b)
+a = 10
+if a > 0:
+    print("manzana")
+else:
+    print("naranja")
+# Salida: manzana
+
+# c)
+a = 10
+if a > 0:
+    print("Te quiero")
+    print("bien lejos.")
+# Salida: Te quiero bien lejos.
+
+# d)
+a = 5
+b = 3
+c = 2
+if a < b * c:
+    print("Hola!")
+else:
+    print("Chau!")
+# Salida: Hola!
+
+# e)
+p1 = 3.14
+p2 = 3.141569
+if p1 == p2:
+    print("p1 y p2 son iguales!")
+else:
+    print("p1 y p2 no son iguales!")
+# Salida: p1 y p2 son iguales!
+
+# f)
+a = "Hola"
+b = "hola"
+if a == b:
+    print("Python es insensible!")
+else:
+    print("Python es muy sensible!")
+# Salida: Python es muy sensible!
